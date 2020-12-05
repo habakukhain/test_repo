7f64cfe28ad453d879d6573a9760ee332ecc7b0e
habakukhain <36795531+habakukhain@users.noreply.github.com>
2020-12-05 14:35:51 +0100

    Create issue_generator.py

---
+++ .github/workflows/issue_generator.py
@@ -0,0 +1,20 @@
+import numpy as np
+import json
+import requests
+from github import Github
+import os
+from pprint import pprint
+
+token = os.getenv('github_token')
+g = Github(token)
+repo = g.get_repo("habakukhain/test_repo")
+
+i = repo.create_issue(
+    title="Issue Title",
+    body="Text of the body.",
+    assignee="habakukhain",
+    labels=[
+        repo.get_label("good first issue")
+    ]
+)
+pprint(i)
