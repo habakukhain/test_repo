2d39cd6d369552eb02162527a17a902246723847
habakukhain <36795531+habakukhain@users.noreply.github.com>
2020-12-05 14:38:59 +0100

    Update issue_generator.py

--- .github/workflows/issue_generator.py
+++ .github/workflows/issue_generator.py
@@ -5,7 +5,7 @@ from github import Github
 import os
 from pprint import pprint
 
-token = os.getenv('github_token')
+token = os.getenv('git_token')
 g = Github(token)
 repo = g.get_repo("habakukhain/test_repo")
 
